31261691494c08b8ba078a796631b0a0751c2623
Marcelo Bezerra <marcelo.bezerra@criarenet.com>
2021-06-24 16:27:45 -0300

    update php

--- src/RecurrentCreditCardPayment.php
+++ src/RecurrentCreditCardPayment.php
@@ -4,15 +4,10 @@ namespace Mjbezer\Cielo;
 
 use Cielo\API30\Merchant;
 
-use Cielo\API30\Ecommerce\Environment;
 use Cielo\API30\Ecommerce\Sale;
 use Cielo\API30\Ecommerce\CieloEcommerce;
 use Cielo\API30\Ecommerce\Payment;
-use Cielo\API30\Ecommerce\CreditCard;
-use Cielo\API30\Ecommerce\RecurrentPayment;
 use Cielo\API30\Ecommerce\Request\CieloRequestException;
-
-
 class RecurrentCreditCardPayment{
 
 
